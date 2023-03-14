18c507d533d7bd51e8f5107f5aa2452be5e9457c
Joost Schuur <jschuur@jschuur.com>
2023-03-14 14:27:16 +0000

    made it fancier

--- README.md
+++ README.md
@@ -6,4 +6,11 @@ Inspired by a [tweet](https://twitter.com/joostschuur/status/1635548218518847491
 
 ## Usage
 
-TBD
+1. Customize `AUTHOR`, `CODE_EDITOR`, `PROJECT_DIR` and preferred default `DESCRIPTION` ('Hey dummy, fill this out!' has a nice ring to it).
+2. Add to your existing `.zshrc` or `.bashrc` file
+3. Or create a new `project.sh` and `source project.sh` from your RC file (adjust path based on location)
+4. Optionally create a preferred `README.md` footer in `~/.config/project.sh/README_footer.md`
+
+Now let your imagination run wild and adapt it to your own stack and project preferences.
+
+\- [Joost Schuur](https://joostschuur.com) ([@joostschuur](https://twitter.com/joostschuur))
--- project.sh
+++ project.sh
@@ -1,11 +1,34 @@
 function project {
-  cd ~/Code/Personal
+  PROJECTS_DIR=~/Code/Personal
+  CONFIG_DIR=~/.config/project.sh
+  # or CODE_EDITOR=$EDITOR for your default editor
+  CODE_EDITOR=code
+  DESCRIPTION="${2:-"A new and exciting project"}"
+  AUTHOR="Your Name <you@email.com>"
+
+  if [ -d "$PROJECTS_DIR/$1" ]; then
+    echo "Opening existing project $1 in your projects directory..."
+    cd $PROJECTS_DIR/$1
+
+    $CODE_EDITOR .
+    return
+  fi
+
+  cd $PROJECTS_DIR
   mkdir -p $1
   cd $1
+
   git init
   npm init -y
-  touch README.md
-  echo 'node_modules' > .gitignore
-  echo '.env' >> .gitignore
-  code .
+  npm pkg set type='module'
+  npm pkg set author=$AUTHOR
+  npm pkg set description=$DESCRIPTION
+  echo -e "# $1\n\n$DESCRIPTION" > README.md
+  if [ -f "$CONFIG_DIR/README_footer.md" ]; then
+    cat $CONFIG_DIR/README_footer.md >> README.md
+  fi
+
+  echo -e "node_modules\n.env" > .gitignore
+
+  $CODE_EDITOR .
 }
